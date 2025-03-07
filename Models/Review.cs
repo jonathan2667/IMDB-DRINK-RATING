d4fb1bfc1663f213897cf4cd0f9c70b87e7074ec
915-Nicola-Razvan-Cristian <razvannicola451@gmail.com>
2025-03-07 17:24:19 +0200

    Main page + some example code

---
+++ Models/Review.cs
@@ -0,0 +1,11 @@
+namespace IMDB_DRINK_RATING.Models
+{
+    public class Review
+    {
+        public string Name { get; set; }=string.Empty;
+        public string ReviewText { get; set; }=string.Empty;
+        public int Rating { get; set; }=0;
+
+        //tbd...
+    }
+}
